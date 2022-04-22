900078b4d417163874164966ac72406d2808bfb4
Fathur Rahman <fathur.rahman@udana.id>
2022-04-23 02:23:21 +0700

    Convert Rest Pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,18 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import ProductView from "../views/ProductView.vue";
+import ShoppingCart from "../views/ShoppingCartView.vue";
+import SuccessView from "../views/SuccessView.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
   {
     path: "/about",
     name: "about",
@@ -15,9 +23,19 @@ const routes = [
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
   {
-    path: "/",
-    name: "home",
-    component: HomeView,
+    path: "/product",
+    name: "product",
+    component: ProductView,
+  },
+  {
+    path: "/cart",
+    name: "cart",
+    component: ShoppingCart,
+  },
+  {
+    path: "/success",
+    name: "success",
+    component: SuccessView,
   },
 ];
 
